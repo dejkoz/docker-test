c4fab98a28a5d5176e517be76237a5c19da1aed0
Dejan <96906009+dejkoz@users.noreply.github.com>
2026-04-29 21:16:50 +0000

    updated

--- testi/t1.sh
+++ testi/t1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $(pwd)
-touch run.sh
+echo '#!/bin/bash' > run.sh
+echo 'echo "Zaganjam Test 1"' >> run.sh
 chmod +x run.sh
 bash ./pripravi_izvajalno_okolje.sh git@github.com:dejkoz/ORV.git
